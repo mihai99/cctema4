9d24c91b20fa6abe26659773f7477b5d0a0ca0a4
mihai99133 <mihai.blaga@amplified.software>
2021-04-19 13:01:15 +0300

    integrated login

--- src/App.js
+++ src/App.js
@@ -1,33 +1,44 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import "./App.css";
 import Dashboard from "./components/Dashboard";
 // import Login from "./components/Login";
 import useToken from "./utils/useToken";
 import UniqueBoard from "./components/UniqueBoard";
+import { AuthenticatedTemplate, UnauthenticatedTemplate, useMsal } from "@azure/msal-react";
+import { loginRequest } from "./utils/authConfig";
+import Navbar from "./components/Navbar";
 
 function App() {
-  const { token, setToken } = useToken();
+ const { instance } = useMsal()
 
-  // if (!token) {
-  //   return <Login setToken={setToken} />;
-  // }
+  const handleLogin = () => {
+    instance.loginPopup(loginRequest).catch(e => console.log(e))
+  }
 
   return (
     <div className="wrapper" style={{ height: "100%" }}>
-      <BrowserRouter>
-        <Switch>
-          <Route path="/board/:id">
-            <UniqueBoard />
-          </Route>
-          <Route path="/">
-            <Dashboard />
-          </Route>
-          {/* <Route path="/preferences">
-            <Preferences />
-          </Route> */}
-        </Switch>
-      </BrowserRouter>
+
+      <AuthenticatedTemplate>
+      <Navbar />
+        <BrowserRouter>
+          <Switch>
+            <Route path="/board/:id">
+              <UniqueBoard />
+            </Route>
+            <Route path="/">
+              <Dashboard />
+            </Route>
+            {/* <Route path="/preferences">
+              <Preferences />
+            </Route> */}
+          </Switch>
+        </BrowserRouter>
+      </AuthenticatedTemplate>
+      <UnauthenticatedTemplate>
+        <h4>Please log in</h4>
+        <button onClick={() => handleLogin()}>Log in</button>
+      </UnauthenticatedTemplate>
     </div>
   );
 }
