6eccba4f31f59c999d87dae01549a15e6d2a0f19
mihai99133 <mihai.blaga@amplified.software>
2021-04-20 16:45:20 +0300

    god

--- src/utils/authConfig.js
+++ src/utils/authConfig.js
@@ -14,8 +14,8 @@ export const msalConfig = {
     auth: {
         clientId: "20b25dfb-f25b-413d-aa04-133a96b49af2",
         authority: "https://login.microsoftonline.com/consumers",
-        redirectUri: "https://cc-tema-4-zure.azurewebsites.net/",
-        postLogoutRedirectUri: "https://cc-tema-4-zure.azurewebsites.net/"
+        redirectUri: "http://localhost:3000/",
+        postLogoutRedirectUri: "http://localhost:3000/"
     },
     cache: {
         cacheLocation: "sessionStorage", // This configures where your cache will be stored
